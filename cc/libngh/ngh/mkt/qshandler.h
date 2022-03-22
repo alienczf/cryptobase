06f5b8e5b9af6431403e801f567c6815db7c2b58
zhanfeng-alpha <zhanfeng@alphalab.capital>
2022-03-23 06:15:23 +0800

    alc: add book handling

---
+++ cc/libngh/ngh/mkt/qshandler.h
@@ -0,0 +1,133 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <sstream>
+
+#include "date.h"
+#include "ngh/log.h"
+#include "ngh/types/types.h"
+
+namespace ngh::mkt {
+using namespace data::alc;
+class QsHandler {
+ public:
+  QsHandler(UnixTimeMicro trade_agg_window = 10000)
+      : trade_agg_window(trade_agg_window) {}
+
+  void OnLevel(const bool isBid, const data::alc::BookHdrV2& hdr,
+               const data::alc::PriceLevelV2& lvl) {
+    if ((hdr.seqnum >= seq_num) && (hdr.exch_time >= exch_time)) {
+      const auto idx = isBid ? 0 : 1;
+      if (isnan(lvl.qty) || (lvl.qty == 0.)) {
+        levels[idx].erase(lvl.prc);
+      } else {
+        levels[idx][lvl.prc] = lvl.qty;
+      }
+      md_id = hdr.md_id;
+      exch_time = std::max(exch_time, hdr.exch_time);
+      qs_send_time = std::max(qs_send_time, hdr.qs_send_time);
+      seq_num = std::max(seq_num, hdr.seqnum);
+    } else {
+      LOGINF("got stale price: current[%lu] received[%lu] md_id[%lu]",
+             exch_time, hdr.exch_time, hdr.md_id);
+    }
+  }
+
+  void OnTrade(const MD_ID mdId, const UnixTimeMicro qsTs,
+               const UnixTimeMicro exchTs, uint8_t makerSide, Px prc, Qty qty) {
+    const bool continued_trade = ((last_trade_maker_side == makerSide) &&
+                                  (exchTs - exch_time < trade_agg_window));
+    const auto prev_qty = last_trade_qty;
+    last_trade_qty = qty + continued_trade * last_trade_qty;
+    last_trade_prc =
+        (prc * qty + continued_trade *
+                         (isnan(last_trade_prc) ? 0. : last_trade_prc) *
+                         prev_qty) /
+        last_trade_qty;
+    last_trade_maker_side = makerSide;
+    md_id = std::max(md_id, mdId);
+    // these updates could be stale
+    // TODO(ANY): retroactively shift this to l2 print
+    exch_time = std::max(exch_time, exchTs);
+    last_trade_time = std::max(last_trade_time, exchTs);
+    qs_send_time = std::max(qs_send_time, qsTs);
+    if (exchTs < exch_time) {
+      LOGINF("got stale trade: current[%lu] received[%lu] md_id[%lu]",
+             exch_time, exchTs, mdId);
+    } else {
+      FlushBook(makerSide, prc, qty);
+    }
+  }
+
+  void OnTrade(const data::alc::TradeV2& t) {
+    OnTrade(t.md_id, t.qs_send_time, t.exch_time, t.maker_side, t.prc, t.qty);
+  }
+
+  void OnTrade(const data::alc::TradeHdrV3& hdr, const data::alc::TradeV3& t) {
+    OnTrade(hdr.md_id, hdr.qs_send_time, hdr.exch_time, hdr.maker_side, t.prc,
+            t.qty);
+  }
+
+  void FlushBook(uint8_t makerSide, Px prc, Qty qty) {
+    if (makerSide > 0) {  // bids traded
+      for (auto it = levels[0].rbegin(); it != levels[0].rend(); ++it) {
+        if (it->first > prc) {  // bids higher than traded prices
+          levels[0].erase(std::next(it).base());
+        } else if (it->first == prc) {
+          it->second -= qty;
+          if (it->second <= 0.) {
+            levels[0].erase(std::next(it).base());
+          }
+          break;
+        } else {
+          break;
+        }
+      }
+    } else {  // asks traded
+      for (auto it = levels[1].begin(); it != levels[1].end(); ++it) {
+        if (it->first < prc) {  // asks lower than traded prices
+          levels[0].erase(it);
+        } else if (it->first == prc) {
+          it->second -= qty;
+          if (it->second <= 0.) {
+            levels[0].erase(it);
+          }
+          break;
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+  void OnFunding(const data::alc::FundingRateV3& fr) {
+    // TODO(ANY): implement
+    LOGDBG(
+        "FundingRateV3: qs_latency: %d, qs_send_time: %lu, "
+        "exch_time: %lu, md_id: %lu, current_funding_rate: %f, "
+        "current_funding_time: %lu, next_funding_rate: %f, "
+        "next_funding_time: %lu, index_price: %f, mark_price: %f",
+        fr.qs_latency, fr.qs_send_time, fr.exch_time, fr.md_id,
+        fr.current_funding_rate, fr.current_funding_time, fr.next_funding_rate,
+        fr.next_funding_time, fr.index_price, fr.mark_price);
+  }
+
+  // config
+  const UnixTimeMicro trade_agg_window;
+
+  // ordering
+  SeqNum seq_num{0};
+  UnixTimeMicro qs_send_time{0};
+  UnixTimeMicro exch_time{0};
+  MD_ID md_id{0};
+  // book
+  PriceBook levels[2];
+  // trade
+  int8_t last_trade_maker_side{true};
+  UnixTimeMicro last_trade_time{0};  // exchange time
+  Px last_trade_prc{NAN};
+  Qty last_trade_qty{0.};
+};
+
+}  // namespace ngh::mkt
--- cc/libngh/ngh/types/types.h
+++ cc/libngh/ngh/types/types.h
@@ -44,7 +44,6 @@ using Balances = std::map<std::string, Qty>;
 using PriceBook = std::map<Px, Qty>;
 using OrderBook = std::map<OID, Order>;
 
-
 struct Ref {
   using Qty = int32_t;
   using Px = int32_t;
@@ -77,5 +76,203 @@ struct Ref {
   }
 };
 using Refs = std::vector<Ref>;
+
+namespace data::alc {
+using UnixTimeNano = int64_t;
+using UnixTimeMicro = int64_t;
+using UnixTimeMilli = int64_t;
+using EpochFracSecs = double;
+
+using DurationNano = int64_t;
+using DurationMicro = int64_t;
+using DurationMilli = int64_t;
+using DurationSecs = double;
+
+using MD_ID = uint64_t;
+using SeqNum = int32_t;
+using VersionHdr = uint8_t;
+using FlagsHdr = uint8_t;
+
+// bitfields
+struct __attribute__((packed)) StreamType {
+  unsigned int value : 7;
+  unsigned int : 1;
+  bool full() const { return value == 0; }
+  bool l1_snapshot() const { return value == 1; }
+  friend std::ostream& operator<<(std::ostream& os, const StreamType& st) {
+    if (st.full()) {
+      os << "full";
+    } else if (st.l1_snapshot()) {
+      os << "l1_snapshot";
+    } else {
+      os << "unknown";
+    }
+    return os;
+  }
+};
+struct __attribute__((packed)) Version {
+  unsigned int value : 7;
+  unsigned int : 1;
+  friend std::ostream& operator<<(std::ostream& os, const Version& v) {
+    os << static_cast<int>(v.value);
+    return os;
+  }
+};
+struct __attribute__((packed)) MsgTypeValue {
+  unsigned int value : 7;
+  unsigned int filtered : 1;
+  friend std::ostream& operator<<(std::ostream& os, const MsgTypeValue& v) {
+    if (v.filtered) {
+      os << "filtered";
+    } else {
+      os << static_cast<int>(v.value);
+    }
+    return os;
+  }
+};
+
+enum MsgTypeId {
+  SnapshotMsg,           // v1, v2, v3
+  DeltaMsg,              // v1, v2, v3
+  TradeMsg,              // v2
+  TradeListMsg,          // v1, v3
+  InferredTradeListMsg,  // v3
+  NumMsgTypes,           // v2, v3
+  FundingRateMsg,
+  UnknownMsg
+};
+
+struct __attribute__((packed)) Header {
+  static constexpr int Version_FundingRate = 100;
+  uint16_t _size;
+  uint16_t stype;
+  uint64_t ts;
+  Version version;
+  StreamType stream_type;
+  uint16_t exch;
+  uint32_t symbol;
+  MsgTypeValue _msg_type;
+  uint8_t qs_type;
+  uint16_t size() const { return _size - sizeof(Header); }
+  bool filtered() const { return _msg_type.filtered; }
+  MsgTypeId msg_type() const {
+    switch (_msg_type.value) {
+      case 0:
+        return SnapshotMsg;
+      case 1:
+        return DeltaMsg;
+      case 2: {
+        switch (version.value) {
+          case 1:
+            return TradeListMsg;
+          case 2:
+            return TradeMsg;
+          case 3:
+            return TradeListMsg;
+        }
+      }
+      case 3:
+        return (version.value == 2) ? NumMsgTypes : InferredTradeListMsg;
+      case 4:
+        return (version.value == Version_FundingRate) ? FundingRateMsg
+                                                      : NumMsgTypes;
+    }
+    return UnknownMsg;
+  }
+  friend std::ostream& operator<<(std::ostream& os, const Header& h) {
+    os << "size: " << h.size() << ", stype: " << h.stype << ", ts: " << h.ts
+       << ", version: " << static_cast<int>(h.version.value)
+       << ", stream_type: " << h.stream_type << ", exch: " << h.exch
+       << ", symbol: " << h.symbol << ", msg_type: " << h.msg_type()
+       << ", qs_type: " << h.qs_type;
+    return os;
+  }
+};
+
+struct __attribute__((packed)) BookHdrV2 {
+  int16_t n_bids, n_asks, num_levels;
+  int32_t qs_latency;  // qs_send_time - qs_recv_time
+  SeqNum seqnum;
+  UnixTimeMicro qs_send_time, exch_time;
+  MD_ID md_id;
+  friend std::ostream& operator<<(std::ostream& os, const BookHdrV2& h) {
+    os << "n_bids: " << h.n_bids << ", n_asks: " << h.n_asks
+       << ", num_levels: " << h.num_levels << ", qs_latency: " << h.qs_latency
+       << ", seqnum: " << h.seqnum << ", qs_send_time: " << h.qs_send_time
+       << ", exch_time: " << h.exch_time << ", md_id: " << h.md_id;
+    return os;
+  }
+};
+
+struct __attribute__((packed)) TradeHdrV3 {
+  int8_t maker_side;
+  int16_t n_trades;
+  int32_t qs_latency;  // qs_send_time - qs_recv_time
+  UnixTimeMicro qs_send_time, exch_time;
+  MD_ID md_id;
+  friend std::ostream& operator<<(std::ostream& os, const TradeHdrV3& h) {
+    os << "maker_side: " << h.maker_side << ", n_trades: " << h.n_trades
+       << ", qs_latency: " << h.qs_latency
+       << ", qs_send_time: " << h.qs_send_time << ", exch_time: " << h.exch_time
+       << ", md_id: " << h.md_id;
+    return os;
+  }
+};
+
+struct __attribute__((packed)) TradeV2 {
+  int8_t maker_side;
+  int32_t qs_latency;  // qs_send_time - qs_recv_time
+  double prc, qty;
+  UnixTimeMicro qs_send_time, exch_time;
+  MD_ID md_id;
+  friend std::ostream& operator<<(std::ostream& os, const TradeV2& t) {
+    os << "maker_side: " << t.maker_side << ", qs_latency: " << t.qs_latency
+       << ", prc: " << t.prc << ", qty: " << t.qty
+       << ", qs_send_time: " << t.qs_send_time << ", exch_time: " << t.exch_time
+       << ", md_id: " << t.md_id;
+    return os;
+  }
+};
+
+struct __attribute__((packed)) PriceLevelV2 {
+  double prc, qty;
+  int16_t cnt;
+  friend std::ostream& operator<<(std::ostream& os, const PriceLevelV2& p) {
+    os << "prc: " << p.prc << ", qty: " << p.qty << ", cnt: " << p.cnt;
+    return os;
+  }
+};
+
+struct __attribute__((packed)) TradeV3 {
+  double prc, qty;
+  friend std::ostream& operator<<(std::ostream& os, const TradeV3& t) {
+    os << "prc: " << t.prc << ", qty: " << t.qty;
+    return os;
+  }
+};
+
+struct __attribute__((packed)) FundingRateV3 {
+  int32_t qs_latency;  // qs_send_time - qs_recv_time
+  UnixTimeMicro qs_send_time, exch_time;
+  MD_ID md_id;
+  double current_funding_rate;
+  UnixTimeMicro current_funding_time;
+  double next_funding_rate;
+  UnixTimeMicro next_funding_time;
+  double index_price;
+  double mark_price;
+  friend std::ostream& operator<<(std::ostream& os, const FundingRateV3& f) {
+    os << "qs_latency: " << f.qs_latency << ", qs_send_time: " << f.qs_send_time
+       << ", exch_time: " << f.exch_time
+       << ", current_funding_rate: " << f.current_funding_rate
+       << ", current_funding_time: " << f.current_funding_time
+       << ", next_funding_rate: " << f.next_funding_rate
+       << ", next_funding_time: " << f.next_funding_time
+       << ", index_price: " << f.index_price
+       << ", mark_price: " << f.mark_price;
+    return os;
+  }
+};
+}  // namespace data::alc
 
 }  // namespace ngh
